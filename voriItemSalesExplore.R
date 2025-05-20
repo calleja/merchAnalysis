a865a56dada37ea00377b173e868d6df96ec4193
Luis Calleja <lecn99@hotmail.com>
2025-05-20 19:12:37 -0400

    completed some analysis

--- voriItemSalesExplore.R
+++ voriItemSalesExplore.R
@@ -2,10 +2,9 @@ library(tidyverse)
 library(randomForest)
 library(fpp3)
 library(ggplot2)
-library(zoo)
 library(lubridate)
 
-setwd('/home/candela/pCloudDrive/pCloud Backup/mofongo-HP-EliteBook-840-G8-Notebook-PC/Documents/ghfc/merch')
+setwd('/home/candela/pCloudDrive/pCloud Backup/mofongo-HP-EliteBook-840-G8-Notebook-PC/Documents/ghfc/merch/merch_analysis_git')
 
 dir()
 getwd()
@@ -29,6 +28,7 @@ items.2 |>
 week(items.2$order.dt)[1:4]
 isoweek(items.2$order.dt)[1:4]
 
+#weekly sales by Barcode
 items.2 |>
   filter(Sold.By.Weight == "false") |>
   group_by(Barcode,year(order.dt),isoweek(order.dt)) |>
@@ -38,4 +38,51 @@ items.2 |>
 
 head(item.summary)
 
+#study to count the number of weeks and range for sales data by Barcode; NOTE the date range of the dataset is limited
+item.summary |>
+  group_by(Barcode) |>
+  summarize(cnt = n(), first = min(year), last = max(year)) -> item.cnt
+
+item.cnt |>
+  arrange(desc(cnt),desc(first)) |>
+  head()
+head()
+
+#verify no NA
+sapply(items.2, function(x) sum(is.na(x)))
+
 #study to look at those barcodes with multiple vendors
+items.2 |>
+  group_by(Department,Barcode) |>
+  summarize(vendor.cnt = length(unique(Active.Vendor))) |>
+  #count # of Barcodes having > 1 vendor
+  filter(vendor.cnt>1) |>
+  group_by(Department) |>
+  summarize(cnt = n()) -> multi.vendor.cnt
+
+#some steps to impute the count of barcodes w/multiple vendors to the "proportion" barchart
+zero.vec <- vendor.prop$Department[!vendor.prop$Department %in% multi.vendor.cnt$Department]
+step.df <- data.frame(zero.vec,rep(0,length(zero.vec)))
+names(step.df) <- names(multi.vendor.cnt)
+df.2.merge <- rbind(step.df,multi.vendor.cnt)
+
+#proportion of Barcodes having > 1 vendor
+items.2 |>
+  group_by(Department,Barcode) |>
+  summarize(vendor.cnt = length(unique(Active.Vendor))) |>
+  #count # of Barcodes having > 1 vendor
+  group_by(Department) |>
+  summarize(cnt = sum(vendor.cnt>1)/n()) -> vendor.prop
+  
+#sort the barcode count dataframe to match the order of Department of vendor.prop
+df.2.merge<- df.2.merge[match(vendor.prop$Department, df.2.merge$Department),]
+
+random_colors <- sample(colors(), length(vendor.prop$Department))
+
+vendor.prop |>
+  ggplot(aes(x=Department,y=cnt, fill=random_colors)) +
+  geom_bar(stat="identity") +
+  theme(axis.text.x = element_text(angle = 45, vjust=0.5)) +
+  labs(title = "Proportion of barcodes with multiple vendors", y="num of barcodes") +
+  geom_text(aes(label = df.2.merge$cnt), vjust = -0.5, position = position_dodge(0.9)) +
+  scale_fill_identity()
